0f6e77e8050e2d3a0cf03cf7fd947c861ba4c861
timoxoszt <20520810@gm.uit.edu.vn>
2022-04-08 21:06:47 -0700

    fixed

--- sample_AES_CBC.cpp
+++ sample_AES_CBC.cpp
@@ -1,6 +1,8 @@
 // g++ -g3 -ggdb -O0 -DDEBUG -I/usr/include/cryptopp Driver.cpp -o Driver.exe -lcryptopp -lpthread
 // g++ -g -O2 -DNDEBUG -I/usr/include/cryptopp Driver.cpp -o Driver.exe -lcryptopp -lpthread
 
+#include <iomanip>
+#include "cryptopp/modes.h"
 #include <cryptopp/files.h>
 using CryptoPP::FileSource;
 using CryptoPP::FileSink;
@@ -8,7 +10,7 @@ using CryptoPP::BufferedTransformation;
 
 #include "cryptopp/osrng.h"
 using CryptoPP::AutoSeededRandomPool;
-using CryptoPP::byte;
+// using CryptoPP::byte;
 
 #include <iostream>
 using std::wcin;
